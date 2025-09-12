40b48c7b5a61ce53d517c95eecfff66036aacdd9
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-09-12 15:27:51 -0500

    feat: configuracion a modulo principal

--- debts-api/src/app.module.ts
+++ debts-api/src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.SUPABASE_HOST,
+      port: +(process.env.SUPABASE_PORT || 5432),
+      username: process.env.SUPABASE_USER,
+      password: process.env.SUPABASE_PASSWORD,
+      database: process.env.SUPABASE_DB,
+      ssl: { rejectUnauthorized: false },
+      autoLoadEntities: true,
+      synchronize: true, //dev
+    }),
+    CacheModule.register({
+      store: redisStore,
+      host: process.env.REDIS_HOST || 'localhost',
+      port: +(process.env.REDIS_PORT || 6379),
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
