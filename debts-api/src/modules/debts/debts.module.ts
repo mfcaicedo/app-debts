7c12c2d00139503ae3ff492a487c2e4331be1cdc
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-09-12 23:18:58 -0500

    feat: se configura servicio de cache en el modulo de deudas

--- debts-api/src/modules/debts/debts.module.ts
+++ debts-api/src/modules/debts/debts.module.ts
@@ -4,11 +4,12 @@ import { DebtOrmEntity } from './infrastructure/debt.orm-entity';
 import { DebtController } from './infrastructure/controllers/debt.controller';
 import { DebtRepositoryImpl } from './infrastructure/respositories/debt-respository-impl';
 import { UsersModule } from '../users/users.module';
+import { CachedDebtService } from './infrastructure/services/cached-debts.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([DebtOrmEntity]), UsersModule],
   controllers: [DebtController],
-  providers: [DebtRepositoryImpl],
-  exports: [DebtRepositoryImpl],
+  providers: [DebtRepositoryImpl, CachedDebtService],
+  exports: [DebtRepositoryImpl, CachedDebtService],
 })
 export class DebtsModule {}
