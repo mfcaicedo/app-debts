00bc77afceef19a6636bbfebe428c925211bbfde
mfcaicedo <mfcaicedo@unicauca.edu.co>
2025-09-13 11:38:50 -0500

    feat: ajuste gatewat de gestion de usuarios

--- debts/src/app/user-management/domain/models/gateway/user-gateway.ts
+++ debts/src/app/user-management/domain/models/gateway/user-gateway.ts
@@ -3,12 +3,8 @@ import { User, UserListPaginated } from "../user.model";
 import { GenericResponse } from "../../../../shared/utils/models/request-response.model";
 
 export abstract class UserGateway {
-    abstract getAllUsersPaginated(): Observable<UserListPaginated[]>;
-    abstract getUserById(id: number): Observable<User>;
-    abstract getUserByUid(uid: string): Observable<User>;
+    abstract getUserByEmail(email: string): Observable<GenericResponse>;
     abstract createUser(user: User): Observable<User>;
     abstract updateUser(user: User): Observable<User>;
     abstract deleteUser(id: number): Observable<User>;
-    abstract getPersonByUserId(userId: number): Observable<GenericResponse>;
-    abstract getTeacherByPersonId(personId: number): Observable<GenericResponse>;
 }
